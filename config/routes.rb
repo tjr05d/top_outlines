a2fffe638169564ab72cc75bd8e63ae33f82556a
tjr05d <tjr05d@my.fsu.edu>
2015-11-26 22:32:01 -0500

    Added the method to the session controller that sets the session id when a user logs in, created flash messages for when a user logs in, and fixed an error in the routes file changing the word user to plural, also added login form in the sessions view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
 
-  get 'signup', to: 'user#new', as: 'signup'
+  get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
