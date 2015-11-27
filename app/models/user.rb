54fd8830ae10326de2de85a27b68d9f8cc135646
tjr05d <tjr05d@my.fsu.edu>
2015-11-26 21:57:09 -0500

    added validations to the user model for first and last name as well as RegEx for email vaildation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   belongs_to :school
   has_secure_password
+  validates :first_name,
+            presence: true
+  validates :last_name,
+            presence: true
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: ^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$ }
 end
