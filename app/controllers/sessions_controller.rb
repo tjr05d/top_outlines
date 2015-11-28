0c57560d852a1e8eee0680a2de66ccf358141b93
tjr05d <tjr05d@my.fsu.edu>
2015-11-28 14:24:56 -0500

    created a new partial, outlinesOwned, the idea is turn the user show page into a dashboard where the current user can see what files they have in the marketplace, they can see there sales, and they can also manage and view the outlines that they have purchase, I would like to make this the place where uploads occur as well, and the place where users manage there stripe account

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: "Welcome to Top Outlines!"
+      redirect_to user, notice: "Welcome to Top Outlines!"
     else
       render new
     end
