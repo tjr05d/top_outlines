62f6f226a9c6a147ee82de1eea1b683fed7ea866
tjr05d <tjr05d@my.fsu.edu>
2015-11-28 12:56:39 -0500

    edited the forms to allow for user to upload and download a file using carrierwave, added bootstrap for some quick styling, will change the styling to become more appealling once the functionality is set better set up

--- app/models/outline.rb
+++ app/models/outline.rb
@@ -1,4 +1,8 @@
 class Outline < ActiveRecord::Base
   belongs_to :course
   belongs_to :user
+  mount_uploader :attachment,
+                AttachmentUploader
+  validates :title,
+            presence: true
 end
