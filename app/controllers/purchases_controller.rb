58aa4856a621cbd65995a80e51f2deddaa816b54
tjr05d <tjr05d@my.fsu.edu>
2015-12-12 21:20:15 -0500

    Sucessfully added the purchase methods to allow a user to purchase an outline from the marketplace

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,31 @@
 class PurchasesController < ApplicationController
   def new
+    @purchase = Purchase.new
     @outline = Outline.find(params[:outline_id])
+    @user = current_user
   end
 
   def create
+    @purchase = Purchase.new(purchase_params)
+    @user = current_user
+    respond_to do |format|
+      if @purchase.save
+        format.html { redirect_to @user, notice: 'Outline was successfully purchased.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @purchase.errors, status: :unprocessable_entity }
+      end
+    end
+
   end
 
   def destroy
+  end
+
+  private
+
+  def purchase_params
+      params.permit(:outline_id, :user_id)
   end
 end
