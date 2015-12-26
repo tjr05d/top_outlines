d1f8eac5d05a51fd1018700bcbd52d2b49a2d6b2
tjr05d <tjr05d@my.fsu.edu>
2015-12-26 18:26:00 -0500

    created a function in the outline model that queries all the courses in all the schools and sets there price based on how many sales the outline has

--- app/models/outline.rb
+++ app/models/outline.rb
@@ -18,4 +18,41 @@ class Outline < ActiveRecord::Base
       "Add to"
     end
   end
+
+  def self.price_audit
+    @schools = School.all
+    @courses = Course.all
+#iterate through every school
+    @schools.each do |school|
+#iterate through every course at the school
+      @courses.each do |course|
+#select the outlines for each course and sort them by sales
+        @outlines = Outline.where(school_id: school.id, course_id: course.id)
+#check to see if the course has any outlines, else sort the array by sales
+        if @sorted_outlines.nil?
+          break
+        else
+          @sorted_outlines = @outlines.sort_by { |outline| outline["sales"]}
+#reverse that array to make it highest sales to lowest
+          @sorted_outlines.reverse!.each do |outline|
+#set the top outlines price to 20 dollars, the 2nd's price to 15, the 3rd's to 10 and the rest to 5 dollars, save to commit the transaction
+            if outline == @sorted_outlines[0]
+              outline.price = "20"
+              outline.save
+            elsif outline == @sorted_outlines[1]
+              outline.price = "15"
+              outline.save
+            elsif outline == @sorted_outlines[2]
+              outline.price = "10"
+              outline.save
+            else
+              outline.price = "5"
+              outline.save
+            end
+          end
+        end
+      end
+    end
+  end
+
 end
